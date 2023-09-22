990987b4ef54b5b6ce919118929dfdb5469841b9
Maksim C <jstars.tech@gmail.com>
2023-09-22 15:02:08 +0300

    Refactor: code reformat

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const fs = require("fs");
 
 function gen(minLong = 1, minLat = 1, maxLong = 1.2, maxLat = 1.2) {
-    let long = Number((Math.random() * (maxLong - minLong) + minLong).toFixed(12));
-    let lat = Number((Math.random() * (maxLat - minLat) + minLat).toFixed(12));
+  const long = Number(
+    (Math.random() * (maxLong - minLong) + minLong).toFixed(12)
+  );
+  const lat = Number((Math.random() * (maxLat - minLat) + minLat).toFixed(12));
 
-    return [long, lat];
+  return [long, lat];
 }
 
 const bbox = [34.2654333839, 29.5013261988, 35.8363969256, 33.2774264593];
 const cordsArr = new Map();
 
 for (let i = 0; i < 1000; i++) {
-    let res;
+  let res;
 
-    do {
-        res = gen(bbox[0], bbox[1], bbox[2], bbox[3]);
-    } while (cordsArr.has(res));
+  do {
+    res = gen(bbox[0], bbox[1], bbox[2], bbox[3]);
+  } while (cordsArr.has(res));
 
-    cordsArr.set(res, 1);
+  cordsArr.set(res, 1);
 }
 
-let geoData = {
-    "type": "FeatureCollection",
-    "features": []
+const geoData = {
+  type: "FeatureCollection",
+  features: [],
 };
 
 for (const cords of Array.from(cordsArr.keys())) {
-    let feature = {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-            "type": "Point",
-            "coordinates": cords
-        }
-    };
+  const feature = {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "Point",
+      coordinates: cords,
+    },
+  };
 
-    geoData.features.push(feature);
+  geoData.features.push(feature);
 }
 
-
-fs.writeFileSync('geoData.json', JSON.stringify(geoData, null, 2));
-
-
-
+fs.writeFileSync("geoData.json", JSON.stringify(geoData, null, 2));
