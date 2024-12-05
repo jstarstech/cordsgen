994ad528eb538adfc7027edf51d5e4d2f284818c
Maksim C <jstars.tech@gmail.com>
2024-12-05 14:57:44 +0300

    Chore: add Prettier configuration and integrate Prettier as a dev dependency

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import { writeFileSync } from "fs";
-import { encodeBase32 } from "geohashing";
+import { writeFileSync } from 'fs';
+import { encodeBase32 } from 'geohashing';
 
 function gen(minLong = 1, minLat = 1, maxLong = 1.2, maxLat = 1.2) {
-  const long = Number(
-    (Math.random() * (maxLong - minLong) + minLong).toFixed(12)
-  );
+  const long = Number((Math.random() * (maxLong - minLong) + minLong).toFixed(12));
   const lat = Number((Math.random() * (maxLat - minLat) + minLat).toFixed(12));
 
   return [long, lat];
@@ -24,16 +22,16 @@ function generateGeoData(bbox, count = 0) {
   }
 
   const geoData = {
-    type: "FeatureCollection",
+    type: 'FeatureCollection',
     features: [],
   };
 
   for (const cords of Array.from(cordsMap.keys())) {
     const feature = {
-      type: "Feature",
+      type: 'Feature',
       properties: {},
       geometry: {
-        type: "Point",
+        type: 'Point',
         coordinates: cords,
       },
     };
@@ -41,7 +39,7 @@ function generateGeoData(bbox, count = 0) {
     geoData.features.push(feature);
   }
 
-  writeFileSync("geoData.json", JSON.stringify(geoData, null, 2));
+  writeFileSync('geoData.json', JSON.stringify(geoData, null, 2));
 }
 
 function generateGeoHashJsonOut(bbox, count = 0) {
@@ -57,7 +55,7 @@ function generateGeoHashJsonOut(bbox, count = 0) {
     cordsArr.push(encodeBase32(res[1], res[0]));
   }
 
-  writeFileSync("geoHashData.json", JSON.stringify(cordsArr, null, 2));
+  writeFileSync('geoHashData.json', JSON.stringify(cordsArr, null, 2));
 }
 
 const bbox = [34.2654333839, 29.5013261988, 35.8363969256, 33.2774264593];
